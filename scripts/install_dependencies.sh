79c2d83415712be55f92163f6394aedd73f7a4e0
marieswarank95 <75885634+marieswarank95@users.noreply.github.com>
2023-08-22 16:48:46 +0530

    Updated install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,6 +2,6 @@
 yum install -y httpd
 if [ "$DEPLOYMENT_GROUP_NAME" == "Development" ]
 then
-    sed -i -e 's/Listen 80/Listen 9090/g' /etc/httpd/conf/httpd.conf
+    sed -i -e 's/Listen 80/Listen 9091/g' /etc/httpd/conf/httpd.conf
 fi
 
